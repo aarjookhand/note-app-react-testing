924002ca2a7fff7511b80e8d03f221cfee934bef
aarjookhand <03aarjoo@gmail.com>
2024-06-14 20:40:12 +0200

    feature delete: initiation

---
+++ frontend/src/views/NotePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import DeleteNote from '../components/DeleteNote';
+
+
+
+
+const NotePage = () => {
+
+    return (
+        <div data-testid="note-page-div">
+            <DeleteNote />  
+        </div>
+    );
+};
+
+export default NotePage;
